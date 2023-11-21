b6ce0e9799d080820a3f101a0589b6e30988348c
Grace Nweke <lagra2001@gmail.com>
2023-11-21 14:08:57 -0800

    solving for task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- a script that prepares a MySQL server for the project:
+-- A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
