83cab3606c58e6ab07cb267b11e37f659cb0d856
Grace Nweke <lagra2001@gmail.com>
2023-11-21 14:06:54 -0800

    trying to resolve task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,5 @@
+-- a script that prepares a MySQL server for the project
+-- A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
