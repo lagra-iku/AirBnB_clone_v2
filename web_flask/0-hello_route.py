a5eb5ddf9ff85b8fde889899a061e1c3036795d4
grace.o.nweke <lagra2001@gmail.com>
2024-01-01 01:27:12 +0000

    correcting task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
-tarts a flask web application
+#!/usr/bin/python3
+# starts a flask web application
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -9,7 +10,7 @@ def hello_hbnb():
     """
     print helo HBNB
     """
-    return "<h1>Hello, HBNB!</h1>"
+    return "Hello HBNB!"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
